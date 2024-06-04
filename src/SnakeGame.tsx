606e195dd5e852a3774b8d48e838813d5baf0f1b
Arber Avdullahu <arbr.avdullahu@gmail.com>
2024-06-04 17:16:48 +0200

    Apply formatter and add initial Game file

---
+++ src/SnakeGame.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import React from "react";
+
+const PageContainer = styled.div`
+  text-align: center;
+`;
+
+const SnakeGame: React.FC = () => {
+  return (
+    <PageContainer>
+      <h1>Snake Game</h1>
+    </PageContainer>
+  );
+};
+
+export default SnakeGame;
